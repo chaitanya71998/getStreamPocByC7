b2229f188ae9ee10d131eab8812a83d5b2d4a6bf
Chaitanya <chaitanya.terli@nxtwave.co.in>
2022-10-17 16:33:32 +0530

    Updates

---
+++ precache-manifest.aef5de8d74e5d99ca584dbad21fee476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "825d065ffa9f8ccb5e3f5080b4c7fec9",
+    "url": "/getStreamPocByC7/index.html"
+  },
+  {
+    "revision": "5d63059f3ce9317ed942",
+    "url": "/getStreamPocByC7/static/css/2.7af04554.chunk.css"
+  },
+  {
+    "revision": "f5f469612ad0107bf73d",
+    "url": "/getStreamPocByC7/static/css/main.11d3e2c1.chunk.css"
+  },
+  {
+    "revision": "5d63059f3ce9317ed942",
+    "url": "/getStreamPocByC7/static/js/2.9b52bd0f.chunk.js"
+  },
+  {
+    "revision": "f5f469612ad0107bf73d",
+    "url": "/getStreamPocByC7/static/js/main.a148c1c9.chunk.js"
+  },
+  {
+    "revision": "17f01d64b0baf00a0598",
+    "url": "/getStreamPocByC7/static/js/runtime~main.2fef06f1.js"
+  }
+]);
